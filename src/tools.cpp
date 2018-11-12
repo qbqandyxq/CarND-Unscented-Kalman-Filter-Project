99cb5952343aa38f9d93a1319990a50ebf4b90ef
qbqandyxq <707307586@qq.com>
2018-11-12 21:04:19 +0900

    calculate the rmse

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,18 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+    VectorXd rmse(4);
+    rmse.fill(0.0);
+    if (estimations.size()!=ground_truth.size()||estimations.size()==0{
+        std::cout<<"Invalid estimation or grounnd_truth data"<<std::endl;
+        return rmse;
+    }
+    for(unsigned int i=0;i<estimations.size();i++){
+        VectorXd residual = estimations(i) - ground_truth(i);
+        residual = residual.array()*residual.array();
+        rmse += residual;
+    }
+    rmse = rmse/estimations.size();
+    rmse = rmse.array().sqrt();
+    return rmse;
 }
