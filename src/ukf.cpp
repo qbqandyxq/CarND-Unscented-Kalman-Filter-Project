409ada2ef88c0177614e780e62832b4a7ff533b0
qbqandyxq <707307586@qq.com>
2018-11-19 19:10:48 +0900

    a

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -26,9 +26,9 @@ UKF::UKF() {
   // initial covariance matrix
   P_ = MatrixXd(5, 5);
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 1.5;
+  std_a_ = 2.0;
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = .5;
+  std_yawdd_ = .3;
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
