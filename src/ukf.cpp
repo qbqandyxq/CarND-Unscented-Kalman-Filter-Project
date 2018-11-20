7b288ca1fc7d6f683a395f86d0bd228d11515e49
qbqandyxq <707307586@qq.com>
2018-11-20 12:12:55 +0900

    1

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -28,7 +28,7 @@ UKF::UKF() {
   // Process noise standard deviation longitudinal acceleration in m/s^2
   std_a_ = 1.;
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 1.;
+  std_yawdd_ = .3;
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -81,12 +81,18 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
             x_(2)=sqrt(pow(vx,2) + pow(vy,2));
             x_(3)=0;
             x_(4)=0;
-            
-            P_ << std_radr_*std_radr_, 0, 0, 0, 0,
-            0, std_radr_*std_radr_, 0, 0, 0,
+            P_ << 0.15, 0, 0, 0, 0,
+            0, 0.15, 0, 0, 0,
             0, 0, 1, 0, 0,
-            0, 0, 0, std_radphi_, 0,
-            0, 0, 0, 0, std_radphi_;
+            0, 0, 0, 1, 0,
+            0, 0, 0, 0, 1;
+            
+            
+//            P_ << std_radr_*std_radr_, 0, 0, 0, 0,
+//            0, std_radr_*std_radr_, 0, 0, 0,
+//            0, 0, 1, 0, 0,
+//            0, 0, 0, std_radphi_, 0,
+//            0, 0, 0, 0, std_radphi_;
             
         }
         else if(meas_package.sensor_type_ == MeasurementPackage::LASER){
